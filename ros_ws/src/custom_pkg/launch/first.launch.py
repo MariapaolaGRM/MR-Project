8e9a0c0461b7c3b2db15a6e9ac8c37cb8104eceb
Mariapaola <mariapaolagerminario@gmail.com>
2025-12-01 13:07:12 +0100

    Update launch files

---
+++ ros_ws/src/custom_pkg/launch/first.launch.py
@@ -0,0 +1,310 @@
+import os
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import AppendEnvironmentVariable
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.actions import GroupAction
+from launch_ros.actions import PushROSNamespace
+from launch_ros.actions import SetRemap
+
+def generate_launch_description():
+    ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+    # Posizioni iniziali 
+    # posizioni per world2
+    x_pose_1 = LaunchConfiguration('x_pose', default='3.0') 
+    y_pose_1 = LaunchConfiguration('y_pose', default='-3.0')
+    x_pose_2 = LaunchConfiguration('x_pose', default='3.0')
+    y_pose_2 = LaunchConfiguration('y_pose', default='5.0')
+
+    # world ufficiale
+    #x_pose_1 = LaunchConfiguration('x_pose', default='2.5') 
+    #y_pose_1 = LaunchConfiguration('y_pose', default='-5.5')
+    #x_pose_2 = LaunchConfiguration('x_pose', default='4.5')
+    #y_pose_2 = LaunchConfiguration('y_pose', default='5.5')
+
+
+    # File
+    world = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'models',
+        'turtlebot3_house',
+        #'house_model.sdf'
+        'house2_model.sdf'
+    )
+
+    model_file1 = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'models',
+        'turtlebot3_burger_cam',
+        'model1.sdf'
+    )
+
+    model_file2 = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'models',
+        'turtlebot3_burger_cam',
+        'model2.sdf'
+    )    
+
+    urdf_path1 = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'urdf',
+        'turtlebot3_burger_cam1.urdf'
+        )
+
+    with open(urdf_path1, 'r') as infp:
+        robot_desc1 = infp.read()
+
+    urdf_path2 = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'urdf',
+        'turtlebot3_burger_cam2.urdf'
+        )
+
+    with open(urdf_path2, 'r') as infp:
+        robot_desc2 = infp.read()
+
+    bridge_params = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'params',
+        'turtlebot3_burger_cam_bridge.yaml'
+    )
+
+    slam_launch = os.path.join(
+        get_package_share_directory('slam_toolbox'),
+        'launch', 'online_async_launch.py'  
+    )
+
+    slam_config_1 = os.path.join(
+        get_package_share_directory('custom_pkg'),
+        'config', 'slam1.yaml'  
+    )
+
+    slam_config_2 = os.path.join(
+        get_package_share_directory('custom_pkg'),
+        'config', 'slam2.yaml' 
+    )
+
+    rviz_config = os.path.join(
+        get_package_share_directory('custom_pkg'),
+        'rviz', 'rviz_config.rviz'  
+    )
+
+
+    # Launch Gazebo simulation
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': ['-r -s -v2 ', world], 'on_exit_shutdown': 'true'}.items()
+    )
+
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': '-g -v2 ', 'on_exit_shutdown': 'true'}.items()
+    )
+
+    set_env_vars_resources = AppendEnvironmentVariable(
+            'GZ_SIM_RESOURCE_PATH',
+            os.path.join(
+                get_package_share_directory('turtlebot3_gazebo'),
+                'models'))
+
+    # Spawn e configurazione dei robot burger_cam
+    spawn_turtlebot_cmd1 = Node(
+        package='ros_gz_sim',
+        executable='create',
+        namespace='robot1',
+        arguments=[
+            '-name', 'robot1',
+            '-file', model_file1,
+            '-x', x_pose_1,
+            '-y', y_pose_1,
+            '-z', '0.01',
+            '-Y', '3.14159'
+        ],
+        output='screen',
+    )
+
+    spawn_turtlebot_cmd2 = Node(
+        package='ros_gz_sim',
+        executable='create',
+        namespace='robot2',
+        arguments=[
+            '-name', 'robot2',
+            '-file', model_file2,
+            '-x', x_pose_2,
+            '-y', y_pose_2,
+            '-z', '0.01',
+            '-Y', '3.14159'
+        ],
+        output='screen',
+    )
+
+    start_gazebo_ros_bridge_cmd = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ],
+        output='screen',
+    )
+
+    start_gazebo_ros_image_bridge_cmd = Node(
+        package='ros_gz_image',
+        executable='image_bridge',
+        arguments=[
+            '/robot1/camera/image_raw',
+            '/robot2/camera/image_raw',
+        ],
+        output='screen',
+    )
+
+    robot_state_publisher_cmd1 = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        namespace='robot1',
+        output='screen',
+        parameters=[{
+            'use_sim_time': use_sim_time,
+            'robot_description': robot_desc1
+        }]
+    )
+
+    robot_state_publisher_cmd2 = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        namespace='robot2',
+        output='screen',
+        parameters=[{
+            'use_sim_time': use_sim_time,
+            'robot_description': robot_desc2
+        }]
+    )    
+    
+
+    # Launch SLAM Toolbox
+    slam1 = GroupAction(
+        actions=[
+            PushROSNamespace('robot1'),
+            SetRemap(src='/map', dst='/robot1/map'),
+            IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(slam_launch),
+            launch_arguments={
+                'use_sim_time': use_sim_time,  
+                'slam_params_file':slam_config_1,
+            }.items()
+            )
+    ])
+
+    slam2 = GroupAction(
+        actions=[
+            PushROSNamespace('robot2'),
+            SetRemap(src='/map', dst='/robot2/map'),
+            IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(slam_launch),
+            launch_arguments={
+                'use_sim_time': use_sim_time, 
+                'slam_params_file':slam_config_2,
+            }.items()
+            )
+    ])
+
+
+    # Trasformazioni statiche per creare il frame map
+    map_broadcatser_cmd_1 = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name="map_broadcaster_1",
+        arguments=[
+            "--x", "3.0", "--y", "-3.0", "--z", "0.01", # world 2
+            #"--x", "3.5", "--y", "-5.5", "--z", "0.01", # world ufficiale
+            "--roll", "0.0", "--pitch", "0.0", "--yaw", "3.14159",
+            "--frame-id", "map", "--child-frame-id", "robot1/map"
+        ],
+        output="screen" 
+    )
+
+    map_broadcatser_cmd_2 = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name="map_broadcaster_2", 
+        arguments=[
+            "--x", "3.0", "--y", "5.0", "--z", "0.01", # world 2
+            #"--x", "5.5", "--y", "5.5", "--z", "0.01", # world ufficiale
+            "--roll", "0.0", "--pitch", "0.0", "--yaw", "3.14159",
+            "--frame-id", "map", "--child-frame-id", "robot2/map"
+        ],
+        output="screen" 
+    )
+
+    # YOLO
+    yolo_node_1 = Node(
+        package='custom_pkg',
+        executable='yolo_node',
+        name='yolo_node',
+        namespace='robot1',
+        parameters=[
+                {'namespace':'robot1'},
+                {"use_sim_time": use_sim_time}],
+        output='screen',
+    )
+
+    yolo_node_2 = Node(
+        package='custom_pkg',
+        executable='yolo_node',
+        name='yolo_node',
+        namespace='robot2',
+        parameters=[
+                {'namespace':'robot2'},
+                {"use_sim_time": use_sim_time}],
+        output='screen',
+    )
+
+    # Rviz2 con parametri
+    rviz2 = Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output='screen')
+    
+
+    # Add the commands to the launch description
+    ld = LaunchDescription()
+
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
+    ld.add_action(set_env_vars_resources)
+    
+    ld.add_action(spawn_turtlebot_cmd1)
+    ld.add_action(spawn_turtlebot_cmd2)
+    ld.add_action(start_gazebo_ros_bridge_cmd)
+    ld.add_action(start_gazebo_ros_image_bridge_cmd)
+    ld.add_action(robot_state_publisher_cmd1)
+    ld.add_action(robot_state_publisher_cmd2)
+
+    ld.add_action(slam1)
+    ld.add_action(slam2)
+
+    ld.add_action(map_broadcatser_cmd_1)
+    ld.add_action(map_broadcatser_cmd_2)
+
+    ld.add_action(yolo_node_1)
+    ld.add_action(yolo_node_2)
+
+    ld.add_action(rviz2)
+
+    return ld
---
+++ ros_ws/src/custom_pkg/launch/second.launch.py
@@ -0,0 +1,149 @@
+import os
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.actions import GroupAction
+from launch_ros.actions import PushROSNamespace
+from launch_ros.actions import SetRemap
+from nav2_common.launch import ReplaceString
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+    # File 
+    nav2_launch = os.path.join(
+        get_package_share_directory('nav2_bringup'),
+        'launch', 'navigation_launch.py'  
+    )
+
+    nav2_config_file_1 = os.path.join(
+        get_package_share_directory('custom_pkg'),
+        'config', 'nav2_config1.yaml'
+    )
+
+    nav2_config_file_2 = os.path.join(
+        get_package_share_directory('custom_pkg'),
+        'config', 'nav2_config2.yaml'  
+    )
+
+    config1 = os.path.join(
+        get_package_share_directory("custom_pkg"), 
+        "config", "explore1.yaml"
+    )
+
+    config2 = os.path.join(
+        get_package_share_directory("custom_pkg"), 
+        "config", "explore2.yaml"
+    )
+    
+    # Nav2
+    nav2_1 = GroupAction(
+            actions=[
+                PushROSNamespace('robot1'),
+                SetRemap(src='/tf', dst='/tf'),
+                SetRemap(src='/tf_static', dst='/tf_static'),
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(nav2_launch),
+                    launch_arguments={
+                        'autostart':'true',
+                        'namespace':'robot1',
+                        'use_sim_time': use_sim_time,
+                        'params_file': nav2_config_file_1, 
+                    }.items()
+                )
+            ]
+    )
+
+    nav2_2 = GroupAction(
+            actions=[
+                PushROSNamespace('robot2'),
+                SetRemap(src='/tf', dst='/tf'),
+                SetRemap(src='/tf_static', dst='/tf_static'),
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(nav2_launch),
+                    launch_arguments={
+                        'autostart':'true',
+                        'namespace':'robot2',
+                        'use_sim_time': use_sim_time,
+                        'params_file': nav2_config_file_2,  
+                    }.items()
+                )
+            ]
+    )
+
+    # Explore lite
+    explore_lite_1 = Node(
+        package="explore_lite",
+        name="explore_node",
+        namespace="robot1",
+        executable="explore",
+        parameters=[config1, 
+                    {"use_sim_time": use_sim_time}],
+        output="screen",
+        # remappings=[("/tf", "/tf"), ("/tf_static", "/tf_static")],
+    )
+
+    explore_lite_2 = Node(
+        package="explore_lite",
+        name="explore_node",
+        namespace='robot2',
+        executable="explore",
+        parameters=[config2, 
+                    {"use_sim_time": use_sim_time}
+                    ],
+        output="screen",
+        arguments=["--ros-args", "--log-level", "robot2.explore_node:=debug"]
+        # remappings=[("/tf", "tf"), ("/tf_static", "tf_static")],
+    )
+
+    # Nodi custom
+    central_node = Node(
+        package='custom_pkg',
+        executable='central_node',
+        name='central_node',
+        parameters=[
+                {"use_sim_time": use_sim_time}],
+        output='screen',
+    )
+
+    goal_node_1 = Node(
+        package='custom_pkg',
+        executable='goal_node',
+        name='goal_node',
+        namespace='robot1',
+        parameters=[
+                {'namespace':'robot1'},
+                {"use_sim_time": use_sim_time}],
+        output='screen',
+    )
+
+    goal_node_2 = Node(
+        package='custom_pkg',
+        executable='goal_node',
+        name='goal_node',
+        namespace='robot2',
+        parameters=[
+                {'namespace':'robot2'},
+                {"use_sim_time": use_sim_time}],
+        output='screen',
+    )
+
+
+    # Add the commands to the launch description
+    ld = LaunchDescription()
+
+    ld.add_action(nav2_1)
+    ld.add_action(nav2_2)
+
+    ld.add_action(explore_lite_1)
+    ld.add_action(explore_lite_2)
+
+    ld.add_action(central_node)
+
+    ld.add_action(goal_node_1)
+    ld.add_action(goal_node_2)
+
+    return ld
