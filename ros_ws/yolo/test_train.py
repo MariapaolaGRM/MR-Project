12b5567e7f2ec0ee33d25c1b685bd9bab8bf678f
Mariapaola <mariapaolagerminario@gmail.com>
2025-12-02 15:42:48 +0100

    Update YOLO

--- ros_ws/yolo/test_train.py
+++ ros_ws/yolo/test_train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("runs/detect/train4/weights/best.pt") 
+model = YOLO("runs/detect/train/weights/best.pt") 
 
 model.predict(source="yolo_train/Img1.png", save=True)
