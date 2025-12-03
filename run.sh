9fe4f0a3f55efa797098b65bb10fcd28bff12557
Mariapaola <mariapaolagerminario@gmail.com>
2025-12-03 14:53:40 +0100

    Update run file

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@ xhost +
 docker run -it --rm --net host --ipc host --privileged \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v ~/.Xauthority:/root/.Xauthority \
-    --runtime nvidia --gpus all \
     -e DISPLAY=$DISPLAY \
     -e XAUTHORITY=$XAUTHORITY \
     -v ./ros_ws/:/root/ros_workspace \
